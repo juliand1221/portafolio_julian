c09c55828bc45e13340a0d29cdbf122242fce6f3
juliand1221 <judt1221@hotmail.com>
2022-07-06 23:36:59 +0200

    Pagina de Acerca

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './styles/App.css';
 import Header from './components/header';
 import Footer_2 from './components/footer_2';
+import Presentacion from './pages/presentacion';
+
 
 
 function App() {
   return (
     <div>
       <Header />
-      
+      <Presentacion />
       <Footer_2/>
       
     </div>
--- src/pages/presentacion.jsx
+++ src/pages/presentacion.jsx
@@ -1,9 +1,20 @@
+import  "../styles/presentacion.css"
+import foto from "../media/redes/Perfil_Julian.jpeg"
 
-
-const presentacion=() => {
+const Presentacion=() => {
     return (
         <div>
-            
+          <div>
+            <img className="foto" src={foto} alt="Perfil"/>
+          </div>
+          <div className="text-presentacion">
+            <b>Me he dedicado al desarrollo web al rededor de un año, primero como un aficionado y ahora como autonomo.
+              Como resultado tengo un solido conocimiento de los principales lenguajes de programación de desarrollo web
+              y del software relacionado, asi como la codificación HTML.
+            </b>
+          </div>
         </div>
     )
 };
+
+export default Presentacion;
