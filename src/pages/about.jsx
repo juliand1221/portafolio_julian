6799de2e412cceaeea3e2e0d94d0524c049b8aa7
juliand1221 <judt1221@hotmail.com>
2022-07-29 22:53:10 +0200

    Arreglo de  About

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -14,10 +14,21 @@ const Curriculum=()=>{
                 About Me
             </div>
             <div className="text-about">
-                <b>Me he dedicado al desarrollo web al rededor de un año, primero como un aficionado y ahora como autónomo.
+                <p>💻 Me he dedicado al desarrollo web al rededor de un año, primero como un aficionado y ahora como autónomo.
                 Como resultado tengo un solido conocimiento de los principales lenguajes de programación de desarrollo web
                 y del software relacionado, así como la codificación HTML.
-                </b>
+                </p>
+                <br></br>
+                <p> 💻  Adquirí conocimientos y experiencia en esta área por medio del programa Misión TIC 2022 del ministerio de telecomunicaciones en Colombia,
+                    el cual fue una especie de Bootcamp donde nos preparaban para ser programadores al tiempo que realizábamos proyectos
+                </p>
+                <br></br>
+                 <p> 💻  Adquirí conocimientos y experiencia en esta área por medio del programa Misión TIC 2022 del ministerio de telecomunicaciones en Colombia,
+                    el cual fue una especie de Bootcamp donde nos preparaban para ser programadores al tiempo que realizábamos proyectos. Adquirí conocimientos y experiencia en esta área por medio del programa Misión TIC 2022 del ministerio de telecomunicaciones en Colombia,
+                    el cual fue una especie de Bootcamp donde nos preparaban para ser programadores al tiempo que realizábamos proyectos. Adquirí conocimientos y experiencia en esta área por medio del programa Misión TIC 2022 del ministerio de telecomunicaciones en Colombia,
+                    el cual fue una especie de Bootcamp donde nos preparaban para ser programadores al tiempo que realizábamos proyectos. Adquirí conocimientos y experiencia en esta área por medio del programa Misión TIC 2022 del ministerio de telecomunicaciones en Colombia,
+                    el cual fue una especie de Bootcamp donde nos preparaban para ser programadores al tiempo que realizábamos proyectos
+                </p>
             </div>
             
             <Footer_2 />
