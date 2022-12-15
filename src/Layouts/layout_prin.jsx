87fcf7934de15f1bacbd447bdf87121f9d9e9cbf
Julian David Tenorio <judt1221@hotmail.com>
2022-12-15 18:21:48 -0500

    Arreglo de letra

--- src/Layouts/layout_prin.jsx
+++ src/Layouts/layout_prin.jsx
@@ -1,15 +1,15 @@
-// import Header from "../components/header"
-// import Footer_2 from "../components/footer_2"
+import Header from "../components/header"
+import Footer_2 from "../components/footer_2"
 
-// const LayoutPrincipal = ({children})=>{
-//     return(
-//         <div className="mainContainer">
-//             <Header/>
-//             <main>{children}</main>
-//             <Footer_2/>
-//         </div>
-//     )
-// };
+const LayoutPrincipal = ({children})=>{
+    return(
+        <div className="mainContainer">
+            <Header/>
+            <main>{children}</main>
+            <Footer_2/>
+        </div>
+    )
+};
 
-// export default LayoutPrincipal;
+export default LayoutPrincipal;
 
